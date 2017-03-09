385031b9ac37838fa0eb0385a98c44c2bf67c3b4
Benjamin <benjaminparkpetersen@gmail.com>
2017-03-09 11:05:01 +0100

    fixes

---
+++ src/w2v.cpp
@@ -0,0 +1,5 @@
+//
+// Created by benjamin on 3/9/17.
+//
+
+#include "w2v.h"
---
+++ src/w2v.h
@@ -0,0 +1,16 @@
+//
+// Created by benjamin on 3/9/17.
+//
+
+#ifndef WORD2VEC_W2V_H
+#define WORD2VEC_W2V_H
+
+
+
+class w2v {
+
+};
+
+
+
+#endif //WORD2VEC_W2V_H
