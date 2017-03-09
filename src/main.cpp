f878085920926682f01e1fce47e94035611ec0c7
Benjamin <benjaminparkpetersen@gmail.com>
2017-03-09 13:22:22 +0100

    Testing the binary file.

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,46 @@
 // Created by benjamin on 3/9/17.
 //
 
+#include <iostream>
+
+#include <string>
+#include "WrapperW2V.h"
+
+using namespace std;
+int main() {
+    cout << "Hello World!" << endl;
+    int dims = WrapperW2V("../word2vecFiles/text8-vector.bin").getNumDimensions();
+    cout << "tihih" + to_string(dims);
+    WrapperW2V wrapper = WrapperW2V("../word2vecFiles/text8-vector.bin");
+ //   float f = wrapper.getWordVectors().front().;
+
+    vector<float> vec = wrapper.getVectorForKnownWord("hi");
+    boost::optional<vector<float>> optional = wrapper.getVectorForWord("hi");
+
+    cout << "The 'hi' vector for knowWord" << endl;
+    for (auto it = vec.begin(); it!=vec.end(); ++it) {
+        cout << to_string(*it.base()) + " ";
+    }
+    cout << endl << "The 'hi' vector for getVectorForWord" << endl;
+    for (auto it = optional.get().begin(); it!=optional.get().end(); ++it) {
+        cout << to_string(*it.base()) + " ";
+    }
+
+    cout << endl << "Testing the Word map" << endl;
+    cout << "First value of 'hi' lookup" + wrapper.getWords().find("hi")->first << endl;
+    cout << "Second val of 'hi' lookup" + to_string(wrapper.getWords().find("hi")->second) << endl;
+
+    cout << "looking up the word 6922 in the wordVectors vector" << endl;
+
+
+    /*
+    for (auto it = wrapper.getWordVectors().begin(); it != wrapper.getWordVectors().end(); ++it) {
+        for (auto it1 = *it.base()->begin(); it1 != *it.base()->end(); ++it1) {
+            cout << "Test" + to_string(it1) << endl;
+
+        }
+    }
+*/
+    return 0;
+
+}
